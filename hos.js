5667d73dc434d33fc8a89bdf018fc3abd9e4731f
Rohan <31851961+rohanthakran@users.noreply.github.com>
2020-02-09 11:16:54 +0530

    Update hos.js

--- hos.js
+++ hos.js
@@ -71,14 +71,5 @@ let slideImage = document.querySelectorAll('.slide'),
           startSlide();
 
 
-          document.getElementById("but").addEventListener("click", function()
-                                               {
-           document.querySelector('.bg-modal').style.display= 'flex';
-           
 
-});
-
-document.querySelector('.close').addEventListener("click", function() {
-    document.querySelector('.bg-modal').style.display= 'none';
-});
 
