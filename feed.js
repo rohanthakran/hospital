7ee36ec6fb9405f0bd846597109f77ba2ad74484
Rohan <31851961+rohanthakran@users.noreply.github.com>
2020-02-10 17:06:18 +0530

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,10 +1,12 @@
  document.getElementById("but").addEventListener("click", function()
                      {
            document.querySelector('.bg-modal').style.display= 'flex';
+           document.body.style.overflow = "hidden";
            
 
 });
 
 document.querySelector('.close').addEventListener("click", function() {
     document.querySelector('.bg-modal').style.display= 'none';
+    document.body.style.overflow = "auto";
 });
